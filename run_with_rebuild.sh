01052334618f4805c1b117f58ec897783c9ab6ef
Ri-Hong <riri.hong@gmail.com>
2025-09-04 21:29:53 -0400

    Specify python path

--- run_with_rebuild.sh
+++ run_with_rebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# Activate venv
+# Activate venv and set Python path
 source /opt/venv/bin/activate
+
+# Add core directories to Python path
+export PYTHONPATH=/workspace:/workspace/core/graph_ssm:$PYTHONPATH
 
 # ---- Rebuild TreeScan ----
 cd /workspace/core/convolutional_graph_ssm/third-party/TreeScan
